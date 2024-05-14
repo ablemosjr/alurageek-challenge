c0cd89a048c4536ccba55e7d7ac7ac1e211616e1
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2024-05-14 13:21:36 -0300

    feat: new product modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('OK');
+// Modal - Adicionar Produto
+const modalCreate = document.getElementById('modalToCreate');
+const btnModalCreate = document.getElementById('openModalToCreate');
+const closeModalCreate = document.getElementById('closeModalToCreate');
+
+btnModalCreate.onclick = function () {
+  modalCreate.style.display = 'block';
+}
+
+closeModalCreate.onclick = function () {
+  modalCreate.style.display = 'none';
+}
